6d66224a399c67d37baa6e16712050cfecc1320d
wxp02 <wxp0025@gmail.com>
2024-11-03 11:53:15 -0500

    Load CSV into BigQuery table

--- dags/retail.py
+++ dags/retail.py
@@ -2,6 +2,12 @@ from airflow.decorators import dag, task
 from datetime import datetime
 
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
+
+from astro import sql as aql
+from astro.files import File
+from astro.sql.table import Table, Metadata
+from astro.constants import FileType
 
 @dag(
     start_date=datetime(2024, 1, 1),
@@ -18,6 +24,27 @@ def retail():
         bucket='jeremy_online_retail',
         gcp_conn_id='gcp',
         mime_type='text/csv',
+    )
+
+    create_retail_dataset = BigQueryCreateEmptyDatasetOperator(
+        task_id='create_retail_dataset',
+        dataset_id='retail',
+        gcp_conn_id='gcp',
+    )
+
+    gcs_to_raw = aql.load_file(
+        task_id='gcs_to_raw',
+        input_file=File(
+            'gs://jeremy_online_retail/raw/online_retail.csv',
+            conn_id='gcp',
+            filetype=FileType.CSV,
+        ),
+        output_table=Table(
+            name='raw_invoices',
+            conn_id='gcp',
+            metadata=Metadata(schema='retail')
+        ),
+        use_native_support=False,
     )
 
 retail()
